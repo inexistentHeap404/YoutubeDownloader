c0dc6a0a77e58ba16b25acadb8e5714617dcf734
Sanjay V <sanjayv0521@gmail.com>
2024-09-14 12:01:27 +0530

    Feature Addition: Now you can enter the path of where you want to download the video

--- ytdgui/app.py
+++ ytdgui/app.py
@@ -6,12 +6,13 @@ from youtubesearchpython import *
 import yt_dlp as dl
 from os import system
 from os import listdir
-
+from os import path
 ## Download functions ##
 def Download():
     video_name_display_var.set(video_name_entry_string.get())
     video_name = video_name_entry_string.get()
     video_type = video_type_variable.get()
+    target = video_target_string.get()
     print(video_type)
     ald = False
     directory = [j.lower() for j in listdir()]
@@ -27,34 +28,42 @@ def Download():
     title = val[0][0].get("title")
     code = val[0][0].get("id")
     if video_type == "0":
-        downloadmp3(video_name, link)
+        downloadmp3(video_name, link, target)
     else:
-        downloadmp4(video_name, link)
+        downloadmp4(video_name, link, target)
 
-def downloadmp3(video_name, link):
+def downloadmp3(video_name, link, target):
     video_download_status_var.set("Download in progress")
-    system("python3 -m yt_dlp -x " + link + " --audio-format mp3 --audio-quality 0")
-    status = system("echo $?")
-    if status == 0:
-        video_download_status_var.set("mp3 Download of video " + video_name + " is successful")
+    if path.exists(target):
+        ##system("python3 -m yt_dlp -x " + link + " --audio-format mp3 --audio-quality 0  -o " + target + "/%(title)s.%(ext)s")
+        system("python3 -m yt_dlp -x " + link + " --audio-format mp3 --audio-quality 0 -o \"" + target + "/%(title)s.%(ext)s\"")
+        status = system("echo $?")
+        if status == 0:
+            video_download_status_var.set("mp3 Download of video " + video_name + " is successful")
+        else:
+            video_download_status_var.set("Download Failed: Unable to download video, try running the script setup.py and try again")
     else:
-        video_download_status_var.set("Download Failed: Unable to download video, try running the script setup.py and try again")
-def downloadmp4(video_name, link):
+       video_download_status_var.set("Download Failed: The given path " + target + " does not exist")
+def downloadmp4(video_name, link, target):
     video_download_status_var.set("Download in progress")
-    system("python3 -m yt_dlp --merge-output-format mp4 -f 137+bestaudio " + link)
-    status = system("echo $?")
-    if status == 0:
-        video_download_status_var.set("mp4 Download of video " + video_name + " is successful")
+    if path.exists(target):
+        ## system("python3 -m yt_dlp --merge-output-format mp4 -f 137+bestaudio  -o " + target + "/%(title)s.%(ext)s " + link)
+        system("python3 -m yt_dlp --merge-output-format mp4 -f 137+bestaudio -o \"" + target + "/%(title)s.%(ext)s\" " + link)
+        status = system("echo $?")
+        if status == 0:
+            video_download_status_var.set("mp4 Download of video " + video_name + " is successful")
+        else:
+            video_download_status_var.set("Download Failed: Unable to download video, try running the script setup.py and try again")
     else:
-        video_download_status_var.set("Download Failed: Unable to download video, try running the script setup.py and try again")
+       video_download_status_var.set("Download Failed: The given path " + target + " does not exist")
 
 
 
 ## YTD SCREEN ##
 window = ttk.Window(themename = "darkly")
 window.title("Youtube Downloader")
-window.geometry("1000x700")
-
+window.geometry("1000x500")
+window.resizable(False, False)
 
 ## Title Label ##
 title_label = ttk.Label(
@@ -71,6 +80,11 @@ input_frame = ttk.Frame(
         master = window
         )
 video_name_entry_string = tk.StringVar()
+video_entry_label = tk.Label(
+        master = input_frame,
+        text = "Enter the video name to download",
+        font = "Calibri 20 bold"
+        )
 video_entry = ttk.Entry(
         master = input_frame,
         textvariable = video_name_entry_string
@@ -87,20 +101,41 @@ for (text, value) in video_type_values.items():
             variable = video_type_variable,
             value = value
             ).pack(pady = 5, padx = 10)
+video_target_string = tk.StringVar()
+video_target_location = ttk.Entry(
+        master = window,
+        text = "/path/to/save/",
+        textvariable = video_target_string
+        )
+
+video_path_entry_label = tk.Label(
+        master = window,
+        text = "Enter the path to save the video",
+        font = "Calibri 20 bold"
+        )
 video_submit_button = ttk.Button(
-        master = input_frame, 
+        master = window, 
         text = "Download",
         command = Download
         )
+video_entry_label.pack()
 video_entry.pack(
-        side = "left", 
         padx = 10
+        )
+input_frame.pack(
+        pady = 20
+        )
+
+video_path_entry_label.pack(
+        padx = 20
+        )
+video_target_location.pack(
+        padx = 20,
         )
 video_submit_button.pack(
-        side = "left", 
-        padx = 10
+        padx = 10,
+        pady = 20
         )
-input_frame.pack()
 
 ## video name display ##
 video_name_display_var = tk.StringVar()
